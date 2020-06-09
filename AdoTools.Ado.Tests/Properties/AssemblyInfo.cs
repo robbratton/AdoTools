e0c54a5673179904eba20c65a1773255300b3153
Robert E. Bratton <rob@robbratton.com>
2020-06-09 16:51:19 -0400

    Update AssemblyInfo.cs

--- AdoTools.Ado.Tests/Properties/AssemblyInfo.cs
+++ AdoTools.Ado.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robert E. Bratton")]
 [assembly: AssemblyProduct("AdoTools.Ado.Tests")]
-[assembly: AssemblyCopyright("Copyright © Robert E. Bratton 2019")]
+[assembly: AssemblyCopyright("Copyright © Robert E. Bratton 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
